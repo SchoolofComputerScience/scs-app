46e59dfcabfeed7400e6ebeccd08ba7110dfb131
Shane Eckel <shaneckel@gmail.com>
2017-06-13 12:19:05 -0400

    Added Search Functionality for both Events and News / scs-redesign/issues#37

--- src/views/EventsListView.vue
+++ src/views/EventsListView.vue
@@ -3,17 +3,25 @@
     <div class="events-view">
       <spinner class="spinner" v-if="!loaded" key="spinner"></spinner>
       <transition name="fade" mode="out-in" v-if="loaded">
-        <div class="card-holder">
-          <div class="card"  v-for="event in events" :key="event.uid" >
-            <div :class="event.type" class="type">{{event.type}}</div>
-            <router-link  :to="'/events/' + event.uid">
-              <div>
-                <h2>{{event.startDate | dateTranslate}}</h2>
-                <h3>{{event.title}}</h3>
-              </div>
-            </router-link>
+        <section class="page">
+          <form class="search" v-on:submit.prevent>
+            <input class="search-input" v-model="query" placeholder="Search Events" @keyup.enter="searchEvents" name="query" autocomplete="off">
+          </form>
+          <div class="card-holder">
+            <div v-if="error" class="error-message">
+              <p>{{error}}</p>
+            </div>
+            <div class="card" v-if="!error" v-for="event in events" :key="event.uid" >
+              <div :class="event.type" class="type">{{event.type}}</div>
+              <router-link  :to="'/events/' + event.uid">
+                <div>
+                  <h2>{{event.startDate | dateTranslate}}</h2>
+                  <h3>{{event.title}}</h3>
+                </div>
+              </router-link>
+            </div>
           </div>
-        </div>
+        </section>
       </transition>
     </div>
   </section>
@@ -35,13 +43,39 @@ export default {
     Spinner
   },
 
+  data () {
+    return {
+      query: '',
+      error: ''
+    }
+  },
+
+  watch: {
+    query: function () {
+      this.searchEvents()
+    }
+  },
+
   computed: {
     loaded() {
+
+      if(this.$store.state.events.error.length > 0){
+        this.error = this.$store.state.events.error
+      }else{
+        this.error = false;
+      }
+
       return this.$store.state.events.list.length > 0 ? true : false
     },
     events() {
       return this.$store.state.events.list
     }
+  },
+
+  methods: {
+    searchEvents: _.debounce(function(){
+      this.$store.dispatch('SEARCH_EVENTS', this.query)
+    }, 500)
   },
 
   beforeMount () {
@@ -66,6 +100,30 @@ export default {
     em
       color #C41230
 
+.error-message{
+  font-size: 1.6em;
+  margin: 1em;
+}
+.search-input{
+  border: none;
+  border-left: .1em solid #C41230;
+  background: white;
+  font-size: 1.5em;
+  font-weight: 300;
+  padding-bottom: .4em;
+  margin-top: 1.6em;
+  outline: none;
+  padding: .5em .2em .5em 1.2em;
+  background: #efefef;
+  transition: border-left .1s, background .1s;
+  width: 100%;
+  margin-bottom: 0em;
+  &:focus{
+    border-left: .2em solid #C41230;
+    background: white;
+    padding-left: .4em
+  }
+}
 .card
   flex 1 calc(50% - 2em)
   margin 1em
--- src/views/NewsListView.vue
+++ src/views/NewsListView.vue
@@ -4,8 +4,14 @@
       <spinner class="spinner" v-if="!loaded" key="spinner"></spinner>
       <transition name="fade" mode="out-in" v-if="loaded">
         <section class="page">
+          <form class="search" v-on:submit.prevent>
+            <input class="search-input" v-model="query" placeholder="Search Articles" @keyup.enter="searchNews" name="query" autocomplete="off">
+          </form>
           <div class="card-holder">
-            <div class="card" :style="{ 'background-image': 'url(' + news_item.image + ')' }" v-for="news_item in news" :key="news_item.uid">
+            <div v-if="error" class="error-message">
+              <p>{{error}}</p>
+            </div>
+            <div class="card" v-if="!error" :style="{ 'background-image': 'url(' + news_item.image + ')' }" v-for="news_item in news" :key="news_item.uid">
               <router-link  :to="'/news/' + news_item.uid">
                 <div class="content">
                   <h2>{{news_item.date | moment("dddd, MMMM Do YYYY")}}</h2>
@@ -22,6 +28,7 @@
 
 <script>
 import Spinner from '../components/Spinner.vue'
+import _ from 'lodash'
 
 function fetchNewsList(store) {
   return store.dispatch('GET_NEWS_LIST')
@@ -36,13 +43,37 @@ export default {
     Spinner
   },
 
+  data () {
+    return {
+      query: '',
+      error: ''
+    }
+  },
+
+  watch: {
+    query: function () {
+      this.searchNews()
+    }
+  },
+
   computed: {
     loaded() {
+      if(this.$store.state.news.error.length > 0){
+        this.error = this.$store.state.news.error
+      }else{
+        this.error = false;
+      }
       return this.$store.state.news.list.length > 0 ? true : false
     },
     news() {
       return this.$store.state.news.list
-    }
+    },
+  },
+
+  methods: {
+    searchNews: _.debounce(function(){
+      this.$store.dispatch('SEARCH_NEWS_ARTICLES', this.query)
+    }, 500)
   },
 
   beforeMount () {
@@ -54,6 +85,30 @@ export default {
 <style lang="stylus" scoped>
 .page{
   display: block;
+}
+.error-message{
+  font-size: 1.6em;
+  margin: 1em;
+}
+.search-input{
+  border: none;
+  border-left: .1em solid #C41230;
+  background: white;
+  font-size: 1.5em;
+  font-weight: 300;
+  padding-bottom: .4em;
+  margin-top: 1.6em;
+  outline: none;
+  padding: .5em .2em .5em 1.2em;
+  background: #efefef;
+  transition: border-left .1s, background .1s;
+  width: 100%;
+  margin-bottom: 1em;
+  &:focus{
+    border-left: .2em solid #C41230;
+    background: white;
+    padding-left: .4em
+  }
 }
 .card-holder {
   display: flex;
