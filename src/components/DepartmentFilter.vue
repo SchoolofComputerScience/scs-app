90e64548cd742290a179c875c657557e1eede94f
Clyde Nelson <clyden@CLYDEN.SP.CS.CMU.EDU>
2017-08-29 14:26:29 -0400

    Fixing Department Filter On Programs Page

--- src/components/DepartmentFilter.vue
+++ src/components/DepartmentFilter.vue
@@ -55,16 +55,22 @@ export default {
   methods: {
     filter(event) {
       let filter_value = event.target.getAttribute('name') || event.target.getAttribute('filter-value');
-      if (filter_value === this.$store.state.department.selected_department){
-        console.log("Was")
-        this.$store.commit("SET_SELECTED_DEPARTMENT", '');
-        if (this.route_link)
-          router.push({ path: '/directory'})
-      } else {
-        console.log("sawd")
-        this.$store.commit("SET_SELECTED_DEPARTMENT", filter_value);
-        if (this.route_link)
-          router.push({ path: '/directory/department/' + filter_value})
+      if (this.route_link) {
+        if (filter_value === this.$store.state.department.selected_department){
+          this.$store.commit("SET_SELECTED_DEPARTMENT", '');
+          if (this.route_link)
+            router.push({ path: '/directory'})
+        } else {
+          this.$store.commit("SET_SELECTED_DEPARTMENT", filter_value);
+          if (this.route_link)
+            router.push({ path: '/directory/department/' + filter_value})
+        }
+      }
+      else {
+        if (filter_value === this.$store.state.department.selected_department)
+          this.$store.commit("SET_SELECTED_DEPARTMENT", '');
+        else
+          this.$store.commit("SET_SELECTED_DEPARTMENT", filter_value);
       }
     }
   }
