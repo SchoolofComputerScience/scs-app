662bc7962958b2a4019b4560714a1e17edd5dcee
Mark Johnson <mark@wahilacreative.com>
2019-01-09 16:43:26 -0500

    making people grid a component

---
+++ src/components/PeopleGrid.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+section(class="page_blocks")
+
+  h2(
+    v-if="hasHeadline"
+    ) People
+  div(class="page_block_container visible")
+
+    PeopleItem(
+      v-for="(person, i) in directory"
+      v-if="i < people_to_show"
+      :person="person"
+      :key="person.scid"
+    )
+    button(
+      class="button_show_more"
+      v-on:click="people_to_show += more_people"
+    ) Show More
+</template>
+
+<script>
+
+import PeopleItem from '../components/PeopleItem.vue';
+
+export default {
+  name: 'peopleGrid-view',
+  props: [
+    'directory',
+    'hasHeadline'
+  ],
+  components: {
+    PeopleItem
+  },
+  data () {
+    return {
+      people_to_show: 8,
+      more_people: 4,
+    }
+  }
+}
+</script>
---
+++ src/components/PeopleItem.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+div(
+  class="page_block person"
+  :style="person.backgroundImage"
+  )
+  router-link(
+    :to="'/people/single/' + person.scid"
+    class="link_absolute"
+    )
+  div(class="page_block_content")
+    span(class="page_block_title") {{ person.display_name }}
+    span(class="page_block_description") {{ person.position }}
+</template>
+
+<script>
+
+export default {
+  name: 'PeopleItem-view',
+  props: [
+    'person'
+  ]
+}
+</script>
