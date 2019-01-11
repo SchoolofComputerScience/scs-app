19c8da6aa5cdf206d4fe489c6992115f23986925
xupit3r <joe@wahilacreative.com>
2019-01-11 13:52:44 -0500

    added ResearchAreaGrid and integrated it into the TabResearchAreas component

---
+++ src/components/ResearchAreaGrid.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+div.filter_item_list.list_cards.visible
+  ResearchAreaItem(
+    v-for="(researchArea, i) in researchAreas"
+    v-if="i < itemsToShow"
+    :researchArea="researchArea"
+    :key="researchArea.area_id"
+  )
+  button.button_show_more(
+    v-if="itemsToShow < researchAreas.length"
+    v-on:click="itemsToShow += moreItems"
+  ) Show More
+</template>
+
+<script>
+
+import ResearchAreaItem from './ResearchAreaItem.vue';
+
+export default {
+  name: 'researchArea-view',
+  props: [
+    'researchAreas',
+    'hasHeadline'
+  ],
+  components: {
+    ResearchAreaItem
+  },
+  data () {
+    return {
+      itemsToShow: 4,
+      moreItems: 2,
+    }
+  }
+}
+</script>
--- src/components/TabResearchAreas.vue
+++ src/components/TabResearchAreas.vue
@@ -1,25 +1,47 @@
 <template lang="pug">
-// filter list for research areas
-
-div(class="filter_item_list list_cards visible")
-  div(
-    v-for="researchArea in contentData"
-    :key="researchArea.area_id"
-    class="list_card"
-    )
-    router-link(
-      :to="'/research/single/' + researchArea.area_id"
-      class="link_absolute"
-    ) {{researchArea.title}}
-    div
-      span(class="item_title") {{researchArea.title}}
+  ResearchAreaGrid(
+    :researchAreas="researchAreas"
+  )
 </template>
 
 <script>
+
+import ResearchAreaGrid from './ResearchAreaGrid.vue';
+
 export default {
   name: 'tab-research_areas',
-  props: [
-    "contentData"
-  ]
+  components: {
+    ResearchAreaGrid
+  },
+  computed: {
+    researchAreas: function () {
+      // TODO: i believe we will have a conditional here...
+      // currently this is only being used on a single 
+      // person page, but it is possible that this tab 
+      // item can be used elsewhere, if so, then it will 
+      // be the responsibility of this function to determine 
+      // where it is being used and return the correct data...
+      
+      return [{
+        area_id: 'machine_learning',
+        title: 'Machine Learning'
+      }, {
+        area_id: 'robotics',
+        title: 'Robotics'
+      }, {
+        area_id: 'human_computer_interaction',
+        title: 'Human-Computer Interaction'
+      }, {
+        area_id: 'artificial_intelligence',
+        title: 'Artificial Intelligence'
+      }, {
+        area_id: 'computer_vision',
+        title: 'Computer Vision'
+      }, {
+        area_id: 'natural_language_processing',
+        title: 'Natural Language Processing'
+      }];
+    }
+  }
 }
 </script>
